857b9ced14187b039f960a0e59b727d7cd49dcb6
Hanifa Putri <rahimahanifa@gmail.com>
2021-10-18 20:26:24 +0700

    change index

--- application/views/index.php
+++ application/views/index.php
@@ -12,7 +12,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>SB Admin 2 - Dashboard</title>
+    <title>TIGABABI</title>
 
     <!-- Custom fonts for this template-->
     <link href="<?php echo base_url() ?>assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">

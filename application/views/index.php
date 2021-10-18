21bfc2a0ffbbdc26f0b83b33bbe0e12d9fc1394d
Ghina Zahirah <ghinzahirah@gmail.com>
2021-10-18 21:00:39 +0700

    change ghina

--- application/views/index.php
+++ application/views/index.php
@@ -12,8 +12,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>TIGABABI</title>
-    <!-- Ini Hani -->
+    <title>TIGABABI JIAYOU</title>
 
     <!-- Custom fonts for this template-->
     <link href="<?php echo base_url() ?>assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
